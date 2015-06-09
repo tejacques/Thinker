87a197d58f7211dda5358c26ab29c7e114720830
Tom Jacques <tejacques@gmail.com>
2015-06-08 21:30:58 -0400

    Expose more in interface

--- interface/App.ts
+++ interface/App.ts
@@ -4,6 +4,8 @@ import Game = require('../lib/Game')
 import GameCard = require('../lib/GameCard')
 import ArrayUtils = require('../lib/ArrayUtils')
 import Range = require('../lib/Range')
+import IterativeDeepening = require('../lib/IterativeDeepening')
+import NegaMax = require('../lib/NegaMax')
 
 var appRoot = document.getElementById('appRoot')
 
@@ -19,5 +21,21 @@ var game = new Game.Game(
     )
 
 var board = React.createElement(Board, { key: 'board', game: game })
+function getNextMove(time) {
+    var game = <Game.Game>window['board'].state.game
+    var color = game.getPlayerId() === 0 ? 1 : -1
+    var node = game.clone()
+    var next = IterativeDeepening(
+        node,
+        NegaMax,
+        9,
+        color,
+        time,
+        null,
+        depthReached => node.turn + depthReached >= 9)
+
+    return next.node.originalNode.move
+}
 
 window['board'] = React.render(board, appRoot)
+window['getNextMove'] = getNextMove
